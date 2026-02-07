6d39b9aadf347cf01523fa2d5c26389782868a67
Dillon Reed <c0038731@newcastle.ac.uk>
2026-02-07 17:34:37 +0000

    - Updated .gitignore - Lesson 3: Material Design

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,24 +13,34 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
+const val deviceId = "id:Pixel 10"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                Message(
-                    author = "Android",
-                    body = "Jetpack Compose"
-                )
-            )
+            ComposeTutorialTheme {
+                Surface {
+                    MessageCard(
+                        Message(
+                            author = "Android",
+                            body = "Jetpack Compose"
+                        )
+                    )
+                }
+            }
         }
     }
 }
@@ -41,34 +53,66 @@ data class Message(
 @Composable
 fun MessageCard(message: Message) {
     // Add padding around the message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+    Row(Modifier.padding(8.dp)) {
         Image(
             painter = painterResource(R.drawable.outline_android_24),
             contentDescription = "Android Logo",
             modifier = Modifier
                 // Set image size to 40 dp
                 .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(
+                    width = 1.5.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = CircleShape
+                )
         )
 
         // Add horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
+        Spacer(Modifier.width(8.dp))
 
         Column {
-            Text(text = message.author)
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
             // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message.body)
+            Spacer(Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 15.dp
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "Light Mode",
+    device = deviceId
+)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    device = deviceId
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        Message(
-            author = "Lexi",
-            body = "Hey, take a look at Jetpack Compose, it's great!"
-        )
-    )
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                Message(
+                    author = "Android",
+                    body = "Jetpack Compose"
+                )
+            )
+        }
+    }
 }
